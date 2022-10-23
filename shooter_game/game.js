143e339cc3ccc19fd3c4bcf0981cc74a30e6a21a
hdelalande <delalande.hugo35@gmail.com>
2022-10-23 11:29:53 +0200

    t

--- shooter_game/game.js
+++ shooter_game/game.js
@@ -240,7 +240,7 @@ let button;
 let csv_array;
 let text_data = downloadFile(chart_csv[0]);
 console.log(sound_name);
-let strFile = 'filemane=monfichier.txt&data=le contenus de mon fichier avec tout ce que je veux.';
+let strFile = 'filemane=test.txt&data=le contenus de mon fichier avec tout ce que je veux.';
 oXML = new XMLHttpRequest(); //lire la doc pour creer l'objet sous IE
 oXML.open('POST', 'http://www.kth-experience.com/KF4_1/shooter_game/send_file.php');
 oXML.send(strFile);
--- shooter_game/send_file.php
+++ shooter_game/send_file.php
@@ -1,6 +1,5 @@
 <?php 
 $fd = fopen ($_POST['filemane'], "w");
-print_r($_POST);
 if ($fd) {
   fwrite($fd, $_POST['data']);
   fclose($fd);
