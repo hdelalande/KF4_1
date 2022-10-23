5be19015b2e5e420843a316ca3aff90e9813b0f3
hdelalande <delalande.hugo35@gmail.com>
2022-10-23 12:28:40 +0200

    test

--- shooter_game/send_file.php
+++ shooter_game/send_file.php
@@ -1,6 +1,6 @@
 <?php 
 $fd = fopen ($_POST['filemane'], "w");
-$fd2 = fopen (test.txt,"w");
+$fd2 = fopen ('test.txt',"w");
 fwrite($fd2, "ok ça marche");
 fwrite($fd, "ok ça marche");
 fwrite($fd2, $_POST['data']);
